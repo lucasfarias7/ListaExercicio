b968668d8f123ac681dda9d84fa626f84953c3f2
Lucas Farias <lucasfarias14@outlook.com>
2020-03-13 16:48:39 -0300

    Atividade 7 em conclusao

---
+++ ListaExercicioATVD7/Entidade/Funcionarios.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaExercicioATVD7.Entidade
+{
+    public class Funcionarios
+    {
+        public string Nome { get; set; }
+
+        public int Matricula { get; set; }
+
+        public string Cpf { get; set; }
+
+        public double Salario { get; set; }
+
+        public double ValeRefeicao { get; set; }
+
+
+        public double SalarioBruto()
+        {
+            return this.Salario + this.ValeRefeicao;
+        }
+
+        
+    }
+}
---
+++ ListaExercicioATVD7/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ListaExercicioATVD7.Entidade;
+
+namespace ListaExercicioATVD7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Funcionarios func1 = new Funcionarios()
+            {
+                Nome = "Lucas Farias",
+                Cpf = "10872637433",
+                Matricula = 201700750,
+                Salario = 2000.00,
+                ValeRefeicao = 100.00
+            };
+
+            func1.SalarioBruto();
+            
+        }
+    }
+}
