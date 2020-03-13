1a89b663bdb4b7ac23741f63d8000e9043b44cb9
Lucas Farias <lucasfarias14@outlook.com>
2020-03-13 15:08:51 -0300

    atividade 5 feita

---
+++ ListaExercicioATVD5/Entidade/Aluno.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaExercicioATVD5.Entidade
+{
+    public class Aluno
+    {
+        public string Nome { get; set; }
+
+        public string Cpf { get; set; }
+
+        public int Matricula { get; set; }
+
+        public string Email { get; set; }
+
+
+        public Aluno()
+        {
+        }
+
+        public Aluno(string nome, string cpf, int matricula, string email)
+        {
+            this.Nome = nome;
+            this.Cpf = cpf;
+            this.Matricula = matricula;
+            this.Email = email;
+        }
+
+
+        public override string ToString()
+        {
+            return "Aluno: "
+                + this.Nome
+                + ", cpf: "
+                + this.Cpf
+                + ", Matricula: "
+                + this.Matricula
+                + ", Email: "+this.Email;
+        }
+    }
+}
---
+++ ListaExercicioATVD5/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ListaExercicioATVD5.Entidade;
+
+namespace ListaExercicioATVD5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+             *  implemente uma classe chamada Aluno. Essa
+                classe deve possuir 4 atributos: nome, cpf, matricula e e-mail.
+                Faça um teste criando objetos da classe Aluno. Altere e exiba no
+                Console os valores armazenados nos atributos desse objeto. Crie
+                uma nova classe chamada TestaAluno trazendo como retorno os
+                atributos de 2 alunos diferentes.
+             * */
+
+            List<Aluno> alunos = new List<Aluno>();
+
+            for(int i=1; i<=2; i++)
+            {
+                Console.WriteLine("Informe os dados do aluno #"+i);
+                Console.WriteLine();
+                Console.Write("nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("cpf: ");
+                string cpf = Console.ReadLine();
+                Console.Write("Matricula: ");
+                int matricula = int.Parse(Console.ReadLine());
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("\n");
+                Aluno aluno = new Aluno(nome, cpf, matricula, email);
+                alunos.Add(aluno);
+            }
+            Console.WriteLine();
+            foreach(Aluno aluno in alunos)
+            {
+                Console.WriteLine(aluno);
+            }
+
+            Console.ReadKey();
+            
+            
+        }
+    }
+}
