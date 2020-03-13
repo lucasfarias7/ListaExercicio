c018fef6e1bcd507f4cb6f9167fd250084191dcb
Lucas Farias <lucasfarias14@outlook.com>
2020-03-13 16:21:16 -0300

    Atividade 6 feita

---
+++ ListaExercicioATVD6/Entidade/Cliente.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaExercicioATVD6.Entidade
+{
+    public class Cliente
+    {
+        public string Nome { get; set; }
+
+        public int Rg { get; set; }
+
+        public string Cpf { get; set; }
+
+        public string Telefone { get; set; }
+
+        public int NumeroConta { get; set; }
+
+
+        public override string ToString()
+        {
+            return "Nome do cliente: "
+                + this.Nome
+                + "\nRg do cliente: "
+                + this.Rg
+                + "\ncpf do cliente: "
+                + this.Cpf
+                + "\nTelefone do cliente: "
+                + this.Telefone
+                + "\ne o numero da conta do cliente "
+                + this.Nome
+                + " é: "
+                + this.NumeroConta
+                +"\n";
+        }
+    }
+}
---
+++ ListaExercicioATVD6/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ListaExercicioATVD6.Entidade;
+
+namespace ListaExercicioATVD6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //numeros randomicos para gerar o numero da conta do cliente
+            Random r = new Random();
+
+            List<Cliente> clientes = new List<Cliente>();
+
+            Cliente lucas = new Cliente()
+            {
+                Nome = "lucas Farias",
+                Rg = 123456,
+                Cpf = "123456789",
+                Telefone = "83981188456",
+                NumeroConta = Convert.ToInt32(r.Next())
+                
+            };
+            
+            clientes.Add(lucas);
+
+            Cliente cliente2 = new Cliente()
+            {
+                Nome = "Teste 2",
+                Rg = 1234,
+                Cpf = "7635454545",
+                Telefone = "88888888",
+                NumeroConta = Convert.ToInt32(r.Next())
+            };
+            
+            clientes.Add(cliente2);
+
+            Cliente cliente3 = new Cliente()
+            {
+                Nome = "cliente 3",
+                Rg = 1234989,
+                Cpf = "7868697",
+                Telefone = "88888888",
+                NumeroConta = Convert.ToInt32(r.Next())
+            };
+
+            clientes.Add(cliente3);
+
+
+            foreach(Cliente cliente in clientes)
+            {
+                Console.WriteLine(cliente);
+            }
+
+
+        }
+    }
+}
